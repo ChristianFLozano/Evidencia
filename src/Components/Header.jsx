154b049e66cbc153e4100d1f330bd717fedb371b
ChristianFLozano <marmolegomarila@gmail.com>
2025-02-28 13:28:43 -0600

    Introduction Unity one completed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,24 @@
 import React from "react";
-
+import { motion } from "motion/react";
 export default function Header(props) {
 
     return (
     <>
         <header className="bg-[#1B2763] text-white text-center p-4 w-full h-38 md:h-40 lg:h-56">
             <div className="w-full h-full bg-gradient-to-bl from-blue-900 to-[#081452] flex items-center justify-center flex-col">
-                <h1 className=" text-2xl sm:text-3xl md:text-5xl lg:text-7xl font-bold">{props.text}</h1>
-                <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-light mt-3">{props.subtitle}</h2>
+                <motion.h1 className=" text-2xl sm:text-3xl md:text-5xl lg:text-7xl font-bold"
+                initial={{ y: -200 }}
+                animate={{ y: 0 }}
+                transition={{ duration: 1.25 }}
+                whileHover={{ scale: 1.1 }}
+                >{props.text}</motion.h1>
+
+                <motion.h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-light mt-3"
+                initial={{ y: -150}}
+                animate={{ y: 0 }}
+                transition={{ duration: 1.25 }}
+                whileHover={{ scale: 1.1 }}
+                >{props.subtitle}</motion.h2>
             </div>
         </header>
     </>
--- src/Pages/Presentation.jsx
+++ src/Pages/Presentation.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import Header from "../Components/Header";
-
+import { motion } from "motion/react";
 export default function Presentation() {
-    return (
-    <section className="w-full h-full">
-        <Header text="Portafolio de evidencias" subtitle="Introducción"/>
-    </section>
+    return ( 
+    <>
+        <section className="w-full h-auto">
+            <Header text="Portafolio de evidencias" subtitle="Introducción"/>
+        </section>
+        <main className="w-screen h-auto bg-[#fff] flex justify-center">
+            <motion.div className=" w-10/12  h-8/12 p-5 mt-14 border-2 border-gray-300 rounded-2xl shadow-2xl mb-5"
+            whileHover={{ scale: 1.05 }}
+            initial={{opacity: 0, x: -100}}
+            animate={{opacity: 1, x: 0}}
+            transition={{duration: 4.5}}
+            >
+                <h3 className="text-md md:text-2xl sm:text-xl " >
+                    En esta pagina se presentara el portafolio de evidencias con las actividades realizadas a lo largo del semestre. <br />
+                    Cada actividad sera presentada en forma de PDF correspondiente a su unidad temática, estas presentaciones serán en forma 
+                    cronológica de la actividad 1 hasta la ultima, en la parte superior se localiza una barra de navegación la cual ayudara
+                    a desplazarse entre las unidades.
+                </h3>
+                <hr />
+                <br />
+                <h2 className="text-lg md:text-3xl sm:text-2xl font-bold">
+                    Unidad 1. Instalacion de sistemas operativos
+                </h2>
+                <h3 className="text-md md:text-2xl sm:text-xl " >
+                En esta primera unidad se vieron los temas relacionados con las instalaciones de sistemas operativos, como puede ser el simple
+                    uso de las memorias booteables, los tipos de sistemas operativos deonde se engloban temas como licencias, distrinuciones e arquitectura
+                    así como maquinas virtuales y la instalación de diversos sistemas operativos dentro de ellas.
+                </h3>
+
+            </motion.div>
+        </main>
+    </>
+    
     );
 }
