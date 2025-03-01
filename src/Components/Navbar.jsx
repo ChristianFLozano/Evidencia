ccf429f637884b7ae0b4e194f232843460f6eb1e
ChristianFLozano <marmolegomarila@gmail.com>
2025-03-01 09:22:52 -0600

    task unit one 80% completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function Navbar() {
+    const location = useLocation();
+
     return (
         <>
             <nav className="w-full h-auto flex flex-wrap justify-evenly p-4 bg-black shadow-lg mb-5">
                 <NavLink
-                    className="text-white text-lg md:text-2xl sm:text-xl font-bold p-2 hover:text-gray-300 transition-colors duration-300"
+                    className={`text-white text-lg md:text-2xl sm:text-xl font-bold p-2 hover:text-gray-300 transition-colors duration-300 ${location.pathname === '/' ? 'underline' : ''}`}
                     to='/'
                     end
                 >
@@ -14,14 +16,12 @@ export default function Navbar() {
                 </NavLink>
 
                 <NavLink
-                    className="text-white text-lg md:text-2xl sm:text-xl font-bold p-2 hover:text-gray-300 transition-colors duration-300"
+                    className={`text-white text-lg md:text-2xl sm:text-xl font-bold p-2 hover:text-gray-300 transition-colors duration-300 ${location.pathname === '/unidaduno' ? 'underline' : ''}`}
                     to="/unidaduno"
                     end
                 >
                     Unidad 1
                 </NavLink>
-
-                
             </nav>
         </>
     );
--- src/maps/UnitOne.js
+++ src/maps/UnitOne.js
@@ -1,20 +1,44 @@
 import actividad1 from '../assets/actividad1.png';
 import actividad2 from '../assets/actividad2.png';
+import actividad3 from '../assets/actividad3.png';
+import actividad4 from '../assets/actividad4.png';
+import actividad5 from '../assets/actividad5.png';
 
 const tasks = [
     {
     id: 1,
     title: "El hardware de la computadora y el sistema operativo",
-    description: "En esta actividad hubo dos temas tratados, el primero fue investigar sobre que era chat gpt y la segunda parte con su ayuda elaborar un diagrama del arranque de una computadora.",
+    description: "En esta actividad primera actividad del semestre fueron tratados dos temas, comenzando con el primero fue investigar sobre que era chat gpt y preguntando a esta misma inteligencia lo que es, la segunda parte también con su ayuda elaborar un diagrama del arranque de una computadora con el proceso brindado por la IA.",
     PDF: "https://drive.google.com/file/d/14EVx9-44DFzMGxmRjkycMqNZBDpVUkP9/view?usp=drive_link",
     img: actividad1
 },
     {
     id: 2,
     title: "Sistemas Operativos y sus tipos",
-    description: "En esta actividad se investigó sobre los sistemas operativos y sus tipos, además de sus funciones y características.",
+    description: "En esta actividad se sentaron las bases sobre lo que era un sistema operativo y cada una de las partes que lo engloban, como lo son las características, tipos y los ejemplos más comunes que encontramos de ellos.",
     PDF: "https://drive.google.com/file/d/1vv-8qDOCOnBMJSCTSZd3U1f7OnX7LDlR/view?usp=drive_link",
     img: actividad2
+    },
+    {
+    id: 3,
+    title: "Configuración de dispositivos",
+    description: "En esta actividad conocimos lo que era una usb booteable así como esta en que casos nos servirá, como actividad principal el objetivo fue bootear la usb con algún sistema operativo de nuestra elección.",
+    PDF: "https://drive.google.com/file/d/1I8XHvHInFnnTzd2j90bBG677xkNZaU0A/view?usp=drive_link",
+    img: actividad3
+    },
+    {
+    id: 4,
+    title: "Dispositivos fisicos",
+    description: "Esta actividad fue realizada en equipo de casi todo el salón, aqui el objetivo era que todos nos organizamos con un objetivo principal el cual era investigar sobre diversos dispositivos de entrada/salida que existen o existieron para complementos de una computadora.",
+    PDF: "https://drive.google.com/file/d/1hvP0Y_0oailURcOR076efcyBb40hLVrk/view?usp=drive_link",
+    img: actividad4
+    },
+    {
+    id: 5,
+    title: "Concepto de maquina virtual y partición de disco",
+    description: "De esta primera unidad encontramos la tarea más larga para realizar, en esta actividad la cual consta de dos partes, la primera es la investigación de lo que es una maquina virtual los tipos que existen y de esto encontrar la que mejor se adapte a nuestros requisitos para la segunda parte, esta segunda parte consta en una máquina virtual crear un multiboot, es decir, añadir 3 sistemas operativos a una solo máquina y que esta tenga un menu para acceder a cada uno de ellos.",
+    PDF: "https://drive.google.com/file/d/1_kJmepFCYquEnf0y7n_QxBwBx2FsJhep/view?usp=drive_link",
+    img: actividad5
     }
 ];
 
